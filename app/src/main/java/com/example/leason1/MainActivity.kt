f35cab452f5483cd2169279dba33ac22ca1ec072
euro11 <p.phicharnvanit11@gmail.com>
2020-02-23 18:03:41 +0700

    first test click button

--- app/src/main/java/com/example/leason1/MainActivity.kt
+++ app/src/main/java/com/example/leason1/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.leason1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonkai.setOnClickListener {
+            val intent = Intent(this, kai::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/leason1/kai.kt
@@ -0,0 +1,15 @@
+package com.example.leason1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class kai : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kai)
+
+    }
+}
